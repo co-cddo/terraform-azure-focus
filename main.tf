ca094d0d46011aeeba60e51547c346795f73d094
mikejwhat <18402492+mikejwhat@users.noreply.github.com>
2025-08-06 16:52:20 +0100

    feat: copy/delete working with private networking

--- main.tf
+++ main.tf
@@ -22,15 +22,11 @@ resource "azurerm_storage_account" "cost_export" {
   account_replication_type = "LRS"
   is_hns_enabled           = true
 
-  # public_network_access_enabled = false
-  public_network_access_enabled = true
+  public_network_access_enabled = false
 
-  # network_rules {
-  #   default_action = "Deny"
-  #   bypass         = ["AzureServices"]
-  # }
   network_rules {
-    default_action = "Allow"
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
   }
 }
 
@@ -46,24 +42,24 @@ resource "azapi_resource" "cost_export" {
   }
 }
 
-# COMMENTED OUT: Private Endpoint for storage account
-# resource "azurerm_private_endpoint" "storage" {
-#   name                = "pe-storage-cost-export"
-#   location            = azurerm_resource_group.cost_export.location
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   subnet_id           = var.subnet_id
-#
-#   private_service_connection {
-#     name                           = "psc-storage-cost-export"
-#     private_connection_resource_id = azurerm_storage_account.cost_export.id
-#     subresource_names              = ["blob"]
-#     is_manual_connection           = false
-#   }
-#
-#   lifecycle {
-#     ignore_changes = [private_dns_zone_group]
-#   }
-# }
+# Private Endpoint for storage account
+resource "azurerm_private_endpoint" "storage" {
+  name                = "pe-storage-cost-export"
+  location            = azurerm_resource_group.cost_export.location
+  resource_group_name = azurerm_resource_group.cost_export.name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "psc-storage-cost-export"
+    private_connection_resource_id = azurerm_storage_account.cost_export.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
 
 resource "azurerm_storage_account" "deployment" {
   name                     = "stcostexdply${random_string.unique.result}"
@@ -73,15 +69,11 @@ resource "azurerm_storage_account" "deployment" {
   account_replication_type = "LRS"
   is_hns_enabled           = true
 
-  # public_network_access_enabled = false
-  public_network_access_enabled = true
+  public_network_access_enabled = false
 
-  # network_rules {
-  #   default_action = "Deny"
-  #   bypass         = ["AzureServices"]
-  # }
   network_rules {
-    default_action = "Allow"
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
   }
 }
 
@@ -109,43 +101,43 @@ resource "azurerm_role_assignment" "grant_sp_deploy_sa_contributor" {
   principal_id         = data.azurerm_client_config.current.object_id
 }
 
-# COMMENTED OUT: Private Endpoint for deployment storage account
-# resource "azurerm_private_endpoint" "deployment" {
-#   name                = "pe-storage-cost-export-deployment"
-#   location            = azurerm_resource_group.cost_export.location
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   subnet_id           = var.subnet_id
-#
-#   private_service_connection {
-#     name                           = "psc-storage-cost-export-deployment"
-#     private_connection_resource_id = azurerm_storage_account.deployment.id
-#     subresource_names              = ["blob"]
-#     is_manual_connection           = false
-#   }
-#
-#   lifecycle {
-#     ignore_changes = [private_dns_zone_group]
-#   }
-# }
+# Private Endpoint for deployment storage account
+resource "azurerm_private_endpoint" "deployment" {
+  name                = "pe-storage-cost-export-deployment"
+  location            = azurerm_resource_group.cost_export.location
+  resource_group_name = azurerm_resource_group.cost_export.name
+  subnet_id           = var.subnet_id
 
-# COMMENTED OUT: Private Endpoint for function app
-# resource "azurerm_private_endpoint" "function_app" {
-#   name                = "pe-func-cost-export"
-#   location            = azurerm_resource_group.cost_export.location
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   subnet_id           = var.subnet_id
-#
-#   private_service_connection {
-#     name                           = "psc-func-cost-export"
-#     private_connection_resource_id = azurerm_function_app_flex_consumption.cost_export.id
-#     subresource_names              = ["sites"]
-#     is_manual_connection           = false
-#   }
-#
-#   lifecycle {
-#     ignore_changes = [private_dns_zone_group]
-#   }
-# }
+  private_service_connection {
+    name                           = "psc-storage-cost-export-deployment"
+    private_connection_resource_id = azurerm_storage_account.deployment.id
+    subresource_names              = ["blob"]
+    is_manual_connection           = false
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
+
+# Private Endpoint for function app
+resource "azurerm_private_endpoint" "function_app" {
+  name                = "pe-func-cost-export"
+  location            = azurerm_resource_group.cost_export.location
+  resource_group_name = azurerm_resource_group.cost_export.name
+  subnet_id           = var.subnet_id
+
+  private_service_connection {
+    name                           = "psc-func-cost-export"
+    private_connection_resource_id = azurerm_function_app_flex_consumption.cost_export.id
+    subresource_names              = ["sites"]
+    is_manual_connection           = false
+  }
+
+  lifecycle {
+    ignore_changes = [private_dns_zone_group]
+  }
+}
 
 # Random string for unique storage account name
 resource "random_string" "unique" {
@@ -180,17 +172,16 @@ resource "azurerm_function_app_flex_consumption" "cost_export" {
   # https://medium.com/p/99ff43c1557f
   # https://github.com/hashicorp/terraform-provider-azurerm/issues/29993?source=post_page-----99ff43c1557f---------------------------------------
   #storage_authentication_type = "SystemAssignedIdentity"
-  storage_authentication_type = "StorageAccountConnectionString"
-  storage_access_key          = azurerm_storage_account.deployment.primary_access_key
-  storage_container_endpoint  = "https://${azurerm_storage_account.deployment.name}.blob.core.windows.net/${azapi_resource.deployment.name}"
-  service_plan_id             = azurerm_service_plan.cost_export.id
-  runtime_name                = "python"
-  runtime_version             = "3.12"
-  maximum_instance_count      = 50
-  instance_memory_in_mb       = 2048
-  https_only                  = true
-  # COMMENTED OUT: VNet integration for function app
-  # virtual_network_subnet_id     = var.function_app_subnet_id
+  storage_authentication_type   = "StorageAccountConnectionString"
+  storage_access_key            = azurerm_storage_account.deployment.primary_access_key
+  storage_container_endpoint    = "https://${azurerm_storage_account.deployment.name}.blob.core.windows.net/${azapi_resource.deployment.name}"
+  service_plan_id               = azurerm_service_plan.cost_export.id
+  runtime_name                  = "python"
+  runtime_version               = "3.12"
+  maximum_instance_count        = 50
+  instance_memory_in_mb         = 2048
+  https_only                    = true
+  virtual_network_subnet_id     = var.function_app_subnet_id
   public_network_access_enabled = var.deploy_from_external_network
 
   identity {
@@ -265,9 +256,7 @@ resource "null_resource" "publish_function_code" {
     publish_code_command = local.publish_code_command
   }
 
-  # COMMENTED OUT: Dependencies on private endpoints
-  # depends_on = [azurerm_function_app_flex_consumption.cost_export, azurerm_role_assignment.grant_sp_deploy_sa_contributor, azurerm_private_endpoint.deployment, azurerm_private_endpoint.function_app]
-  depends_on = [azurerm_function_app_flex_consumption.cost_export, azurerm_role_assignment.grant_sp_deploy_sa_contributor]
+  depends_on = [azurerm_function_app_flex_consumption.cost_export, azurerm_role_assignment.grant_sp_deploy_sa_contributor, azurerm_private_endpoint.deployment, azurerm_private_endpoint.function_app]
 }
 
 # resource "null_resource" "cleanup_external_access" {
@@ -339,11 +328,11 @@ resource "azapi_resource" "daily_cost_export" {
 
 # Get current client config
 data "azurerm_client_config" "current" {}
-# COMMENTED OUT: Virtual network data source (not needed without private networking)
-# data "azurerm_virtual_network" "existing" {
-#   name                = var.virtual_network_name
-#   resource_group_name = var.virtual_network_resource_group_name
-# }
+# Virtual network data source
+data "azurerm_virtual_network" "existing" {
+  name                = var.virtual_network_name
+  resource_group_name = var.virtual_network_resource_group_name
+}
 
 # Get current public IP for external deployment
 # data "http" "current_ip" {
@@ -351,80 +340,80 @@ data "azurerm_client_config" "current" {}
 #   url   = "https://api.ipify.org?format=text"
 # }
 
-# COMMENTED OUT: Private DNS Zones for storage services
-# resource "azurerm_private_dns_zone" "blob" {
-#   name                = "privatelink.blob.core.windows.net"
-#   resource_group_name = azurerm_resource_group.cost_export.name
-# }
-#
-# resource "azurerm_private_dns_zone" "file" {
-#   name                = "privatelink.file.core.windows.net"
-#   resource_group_name = azurerm_resource_group.cost_export.name
-# }
-#
-# resource "azurerm_private_dns_zone" "table" {
-#   name                = "privatelink.table.core.windows.net"
-#   resource_group_name = azurerm_resource_group.cost_export.name
-# }
-#
-# resource "azurerm_private_dns_zone" "sites" {
-#   name                = "privatelink.azurewebsites.net"
-#   resource_group_name = azurerm_resource_group.cost_export.name
-# }
+# Private DNS Zones for storage services
+resource "azurerm_private_dns_zone" "blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = azurerm_resource_group.cost_export.name
+}
 
-# COMMENTED OUT: Private DNS Zone virtual network links
-# resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
-#   name                  = "blob-dns-link"
-#   resource_group_name   = azurerm_resource_group.cost_export.name
-#   private_dns_zone_name = azurerm_private_dns_zone.blob.name
-#   virtual_network_id    = data.azurerm_virtual_network.existing.id
-# }
-#
-# resource "azurerm_private_dns_zone_virtual_network_link" "file" {
-#   name                  = "file-dns-link"
-#   resource_group_name   = azurerm_resource_group.cost_export.name
-#   private_dns_zone_name = azurerm_private_dns_zone.file.name
-#   virtual_network_id    = data.azurerm_virtual_network.existing.id
-# }
-#
-# resource "azurerm_private_dns_zone_virtual_network_link" "table" {
-#   name                  = "table-dns-link"
-#   resource_group_name   = azurerm_resource_group.cost_export.name
-#   private_dns_zone_name = azurerm_private_dns_zone.table.name
-#   virtual_network_id    = data.azurerm_virtual_network.existing.id
-# }
-#
-# resource "azurerm_private_dns_zone_virtual_network_link" "sites" {
-#   name                  = "sites-dns-link"
-#   resource_group_name   = azurerm_resource_group.cost_export.name
-#   private_dns_zone_name = azurerm_private_dns_zone.sites.name
-#   virtual_network_id    = data.azurerm_virtual_network.existing.id
-# }
+resource "azurerm_private_dns_zone" "file" {
+  name                = "privatelink.file.core.windows.net"
+  resource_group_name = azurerm_resource_group.cost_export.name
+}
 
-# COMMENTED OUT: Private DNS A records linking private endpoints to DNS zones
-# resource "azurerm_private_dns_a_record" "storage" {
-#   name                = azurerm_storage_account.cost_export.name
-#   zone_name           = azurerm_private_dns_zone.blob.name
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.storage.private_service_connection[0].private_ip_address]
-# }
-#
-# resource "azurerm_private_dns_a_record" "deployment" {
-#   name                = azurerm_storage_account.deployment.name
-#   zone_name           = azurerm_private_dns_zone.blob.name
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.deployment.private_service_connection[0].private_ip_address]
-# }
-#
-# resource "azurerm_private_dns_a_record" "function_app" {
-#   name                = azurerm_function_app_flex_consumption.cost_export.name
-#   zone_name           = azurerm_private_dns_zone.sites.name
-#   resource_group_name = azurerm_resource_group.cost_export.name
-#   ttl                 = 300
-#   records             = [azurerm_private_endpoint.function_app.private_service_connection[0].private_ip_address]
-# }
+resource "azurerm_private_dns_zone" "table" {
+  name                = "privatelink.table.core.windows.net"
+  resource_group_name = azurerm_resource_group.cost_export.name
+}
+
+resource "azurerm_private_dns_zone" "sites" {
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = azurerm_resource_group.cost_export.name
+}
+
+# Private DNS Zone virtual network links
+resource "azurerm_private_dns_zone_virtual_network_link" "blob" {
+  name                  = "blob-dns-link"
+  resource_group_name   = azurerm_resource_group.cost_export.name
+  private_dns_zone_name = azurerm_private_dns_zone.blob.name
+  virtual_network_id    = data.azurerm_virtual_network.existing.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "file" {
+  name                  = "file-dns-link"
+  resource_group_name   = azurerm_resource_group.cost_export.name
+  private_dns_zone_name = azurerm_private_dns_zone.file.name
+  virtual_network_id    = data.azurerm_virtual_network.existing.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "table" {
+  name                  = "table-dns-link"
+  resource_group_name   = azurerm_resource_group.cost_export.name
+  private_dns_zone_name = azurerm_private_dns_zone.table.name
+  virtual_network_id    = data.azurerm_virtual_network.existing.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "sites" {
+  name                  = "sites-dns-link"
+  resource_group_name   = azurerm_resource_group.cost_export.name
+  private_dns_zone_name = azurerm_private_dns_zone.sites.name
+  virtual_network_id    = data.azurerm_virtual_network.existing.id
+}
+
+# Private DNS A records linking private endpoints to DNS zones
+resource "azurerm_private_dns_a_record" "storage" {
+  name                = azurerm_storage_account.cost_export.name
+  zone_name           = azurerm_private_dns_zone.blob.name
+  resource_group_name = azurerm_resource_group.cost_export.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.storage.private_service_connection[0].private_ip_address]
+}
+
+resource "azurerm_private_dns_a_record" "deployment" {
+  name                = azurerm_storage_account.deployment.name
+  zone_name           = azurerm_private_dns_zone.blob.name
+  resource_group_name = azurerm_resource_group.cost_export.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.deployment.private_service_connection[0].private_ip_address]
+}
+
+resource "azurerm_private_dns_a_record" "function_app" {
+  name                = azurerm_function_app_flex_consumption.cost_export.name
+  zone_name           = azurerm_private_dns_zone.sites.name
+  resource_group_name = azurerm_resource_group.cost_export.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.function_app.private_service_connection[0].private_ip_address]
+}
 
 resource "random_uuid" "app_uuid" {}
 
--- src/cost_export/function_app.py
+++ src/cost_export/function_app.py
@@ -7,6 +7,8 @@ import io
 import boto3
 from azure.identity import ManagedIdentityCredential
 from pyarrow.fs import S3FileSystem
+from azure.storage.blob import BlobServiceClient
+from datetime import datetime, timezone
 
 # Environment variables - examples of expected values:
 client_id = os.environ.get("ENTRA_APP_CLIENT_ID")  # Example: "00000000-0000-0000-0000-000000000000"
@@ -14,30 +16,80 @@ urn = os.environ.get("ENTRA_APP_URN")  # Example: "api://AWS-Federation-App"
 arn = os.environ.get("AWS_ROLE_ARN")  # Example: "arn:aws:iam::000000000000:role/aad_s3"
 target_file_path = os.environ.get("S3_TARGET_PATH")  # Example: "s3://s3bucketname/test/"
 aws_region = os.environ.get("AWS_REGION")  # Example: "eu-west-2"
+storage_connection_string = os.environ.get("STORAGE_CONNECTION_STRING")
+container_name = os.environ.get("CONTAINER_NAME")
  
 app = func.FunctionApp()
  
-@app.blob_trigger(arg_name="costinputdata", path="cost-exports", connection="InputCostDataStorage", source="EventGrid")
-def EventGridBlobTrigger(costinputdata: func.InputStream):
-    logging.info(f"Python blob trigger function processed blob"
-            f"Name: {costinputdata.name}"
-            f"Blob Size: {costinputdata.length} bytes")
-    if (costinputdata.name.endswith('.parquet')):
-        logging.info("Processing parquet file")
-        blob_bytes = costinputdata.read()
-        blob_to_read = io.BytesIO(blob_bytes)
+@app.schedule(schedule="0 0 9 * * *", arg_name="timer", run_on_startup=False, use_monitor=False)
+def daily_cost_export_processor(timer: func.TimerRequest) -> None:
+    """Timer trigger function that runs daily at 9:00 AM UTC to process parquet files"""
+    utc_timestamp = datetime.now(timezone.utc).isoformat()
+
+    if timer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info(f'Daily cost export processor executed at: {utc_timestamp}')
+    
+    try:
+        # Initialize blob service client
+        blob_service_client = BlobServiceClient.from_connection_string(storage_connection_string)
+        container_client = blob_service_client.get_container_client(container_name)
         
-        # read input stream to parquet table
-        table = pq.read_table(blob_to_read)
-
-        ### Any deployment specific requirements can be implemented here ###
-        table = table.drop_columns("ResourceName")
-        ### End of deployment specific requirements ###
-
+        # Get S3 filesystem
         s3 = getS3FileSystem()
-
-        pq.write_to_dataset(table, root_path=f"{target_file_path}/{costinputdata.name}", filesystem=s3, compression='snappy')
-        logging.info("Parquet file written to target")
+        
+        # Find and process all parquet files
+        processed_files = []
+        failed_files = []
+        
+        # List all blobs in container recursively
+        blobs = container_client.list_blobs()
+        
+        for blob in blobs:
+            if blob.name.endswith('.parquet'):
+                try:
+                    logging.info(f"Processing parquet file: {blob.name}")
+                    
+                    # Download blob content
+                    blob_client = container_client.get_blob_client(blob.name)
+                    blob_data = blob_client.download_blob().readall()
+                    blob_to_read = io.BytesIO(blob_data)
+                    
+                    # Read parquet table
+                    table = pq.read_table(blob_to_read)
+                    
+                    ### Any deployment specific requirements can be implemented here ###
+                    table = table.drop_columns("ResourceName")
+                    ### End of deployment specific requirements ###
+                    
+                    # Write to S3
+                    s3_path = f"{target_file_path.rstrip('/')}/{blob.name}"
+                    pq.write_to_dataset(table, root_path=s3_path, filesystem=s3, compression='snappy')
+                    logging.info(f"Successfully uploaded {blob.name} to S3")
+                    
+                    # Delete source file after successful upload
+                    blob_client.delete_blob()
+                    logging.info(f"Successfully deleted source file: {blob.name}")
+                    
+                    processed_files.append(blob.name)
+                    
+                except Exception as e:
+                    logging.error(f"Failed to process {blob.name}: {str(e)}")
+                    failed_files.append({"file": blob.name, "error": str(e)})
+                    continue
+        
+        # Summary logging
+        logging.info(f"Processing complete. Successfully processed: {len(processed_files)} files")
+        if failed_files:
+            logging.warning(f"Failed to process: {len(failed_files)} files - {failed_files}")
+        
+        if not processed_files and not failed_files:
+            logging.info("No parquet files found to process")
+            
+    except Exception as e:
+        logging.error(f"Error in daily cost export processor: {str(e)}")
+        raise
 
 def getS3FileSystem():
     default_credential = ManagedIdentityCredential()
