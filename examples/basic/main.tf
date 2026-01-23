fe79558f1ecbad90db294db9490bdec0134d975d
Warren Ayling <warren.ayling@appvia.io>
2026-01-23 16:08:50 +0000

    chore: example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -18,7 +18,7 @@ module "example" {
   deploy_from_external_network = false
 
   backfill_start_date = "2022-01-01"
-  logging_level       = "DEBUG"
+  logging_level       = "DEBUG"       # INFO (default) or DEBUG
 
   # Uncomment the following line if running in CI/CD with a service principal
   # current_principal_type = "ServicePrincipal"
