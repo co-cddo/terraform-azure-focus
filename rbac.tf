cf9367f8f8782a45f7837b04e7cede199558a9b5
mikejwhat <18402492+mikejwhat@users.noreply.github.com>
2026-06-16 15:41:08 +0000

    use id not full role definition id (we only want the guid)

--- rbac.tf
+++ rbac.tf
@@ -145,7 +145,7 @@ resource "azurerm_role_assignment" "grant_func_storage_account_owner_constrained
     )
     OR
     (
-      @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${data.azurerm_role_definition.storage_blob_data_contributor.role_definition_id}}
+      @Request[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${data.azurerm_role_definition.storage_blob_data_contributor.id}}
     )
   )
   AND
@@ -155,7 +155,7 @@ resource "azurerm_role_assignment" "grant_func_storage_account_owner_constrained
     )
     OR
     (
-      @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${data.azurerm_role_definition.storage_blob_data_contributor.role_definition_id}}
+      @Resource[Microsoft.Authorization/roleAssignments:RoleDefinitionId] ForAnyOfAnyValues:GuidEquals {${data.azurerm_role_definition.storage_blob_data_contributor.id}}
     )
   )
   EOT
