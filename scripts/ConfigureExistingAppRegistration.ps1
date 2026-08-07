bf8d5c0e545ea6a687253dd763daf0cd91a75593
mikejwhat <18402492+mikejwhat@users.noreply.github.com>
2026-08-07 12:25:47 +0000

    fix: add app role creation and assignment logic to ConfigureExistingAppRegistration.ps1

--- scripts/ConfigureExistingAppRegistration.ps1
+++ scripts/ConfigureExistingAppRegistration.ps1
@@ -1,6 +1,5 @@
 #Requires -Modules Microsoft.Graph.Authentication, Microsoft.Graph.Applications
 
-# TODO: Add app role (definition) creation to this script
 # TODO: Update ACTION REQUIRED comment in the check block to just say 'look at respective output for details of how to run ConfigureExistingAppRegistration.ps1': https://github.com/appvia-lz-azure/test-consume-terraform-azure-focus/actions/runs/31128400368/job/92708960926
 # TODO: Replace az rest command in Output with instructions to use this script
 # TODO: Ask Lewis where to add the suffix on the end of the application URI
@@ -13,11 +12,14 @@ param(
 	[string]$AppRegistrationClientID
 )
 
+$ErrorActionPreference = 'Stop'
+
 Import-Module Microsoft.Graph.Applications
 
 $requiredScopes = @('Directory.Read.All', 'AppRoleAssignment.ReadWrite.All', 'Application.Read.All')
 
-$currentScopes = Get-MgContext -ErrorAction SilentlyContinue |
+$mgContext = Get-MgContext -ErrorAction SilentlyContinue
+$currentScopes = $mgContext |
 Select-Object -ExpandProperty Scopes
 
 $currentScopesContainsRequiredScopes = $true
@@ -30,6 +32,7 @@ foreach ($rs in $requiredScopes) {
 
 if (-not ($currentScopesContainsRequiredScopes)) {
 	Connect-MgGraph -Scopes $requiredScopes
+	$mgContext = Get-MgContext
 }
 
 $managedIdentityServicePrincipal = Get-MgServicePrincipal -Filter "AppId eq '$managedIdentityClientID'"
@@ -37,10 +40,29 @@ $managedIdentityServicePrincipal = Get-MgServicePrincipal -Filter "AppId eq '$ma
 $appRegistration = Get-MgApplication -Filter "AppId eq '$AppRegistrationClientID'"
 
 $servicePrincipal = Get-MgServicePrincipal -Filter "AppId eq '$AppRegistrationClientID'"
+
 $appRole = $appRegistration |
 Select-Object -ExpandProperty AppRoles |
 Where-Object -FilterScript { $_.DisplayName -eq 'AssumeRole' -and $_.Value -eq 'AssumeRoleWithWebIdentity' }
 
+if (-not $appRole) {
+	Write-Verbose -Message "Creating app role for app registration with object id: $($appRegistration.Id)..." -Verbose
+
+	$guid = New-Guid | Select-Object -ExpandProperty Guid
+	Update-MgApplication -ApplicationId $appRegistration.Id -AppRoles @{
+		AllowedMemberTypes = @('User', 'Application')
+		Description        = 'My role description'
+		DisplayName        = 'AssumeRole'
+		Id                 = $guid
+		IsEnabled          = $true
+		Value              = 'AssumeRoleWithWebIdentity'
+	}
+
+	$appRegistration = Get-MgApplication -Filter "AppId eq '$AppRegistrationClientID'"
+	$appRole = $appRegistration |
+	Select-Object -ExpandProperty AppRoles |
+	Where-Object -FilterScript { $_.DisplayName -eq 'AssumeRole' -and $_.Value -eq 'AssumeRoleWithWebIdentity' }
+}
 
 $params = @{
 	principalId = $managedIdentityServicePrincipal.Id
@@ -52,5 +74,14 @@ $appRoleAssignment = Get-MgServicePrincipalAppRoleAssignment -ServicePrincipalId
 Where-Object -FilterScript { $_.AppRoleId -eq $appRole.Id }
 
 if (-not ($appRoleAssignment)) {
+	Write-Verbose -Message "Creating app role assignment for managed identity with object id: $($managedIdentityServicePrincipal.Id)..." -Verbose
 	New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $managedIdentityServicePrincipal.Id -BodyParameter $params
+}
+
+$costManagementSuffix = '-' + $($managedIdentityServicePrincipal.DisplayName -split '-')[3]
+$identifierUris = "api://$($mgContext.TenantId)/GDS-AWS-Cost-Forwarding$costManagementSuffix"
+
+if ($identifierUris -ne $appRegistration.IdentifierUris) {
+	Write-Verbose -Message "Setting identifier uris for app registration with object id: $($appRegistration.Id)..." -Verbose
+	Update-MgApplication -ApplicationId $appRegistration.Id -IdentifierUris $identifierUris
 }
